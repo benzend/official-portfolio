5719faa90f30e7c76dcd69b204bcfbb9e492acff
Benjamin <1234hteimaj4321@gmail.com>
2021-11-09 23:14:02 -0700

    fix: test projects now have images and titles data

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import { Nav } from '../components/nav'
-import {useEffect, useState} from 'react'
+import {useEffect, useState} from 'react';
+import { BLogo } from '../components/svg/BLogo'
 import _projects from '../data/projects.json'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 import { ProjectCard } from '../components/ProjectCard'
 
 export default function Home() {
@@ -27,13 +28,21 @@ export default function Home() {
       <Nav />
       <main>
         <section className={styles.heroSection}>
-          <h1 className={styles.mainTitle}>Benjamin Scott</h1>
-          <p className={styles.subHeader}>Web Developer</p>
-          <div className={styles.iconsContainer}>
-            <i className={"fab fa-react " + styles.iconReact}></i>
-            <i className={"fab fa-js-square " + styles.iconJS}></i>
-            <i className={"fas fa-code " + styles.iconCode}></i>
-            <i className={"fab fa-python " + styles.iconPy}></i>
+          <div className={styles.expandingSquareContainer}>
+            <div className={styles.expandingSquare}></div>
+          </div>
+          <div className={styles.heroContent}>
+            <h1 className={styles.mainTitle}>Benjamin Scott</h1>
+            <p className={styles.subHeader}>Web Developer</p>
+            <div className={styles.iconsContainer}>
+              <i className={"fab fa-react " + styles.iconReact}></i>
+              <i className={"fab fa-js-square " + styles.iconJS}></i>
+              <i className={"fas fa-code " + styles.iconCode}></i>
+              <i className={"fab fa-python " + styles.iconPy}></i>
+            </div>
+          </div>
+          <div className={styles.heroLogo}>
+            <BLogo fill='#383838' height="500px" width="500px" />
           </div>
         </section>
         <section>
