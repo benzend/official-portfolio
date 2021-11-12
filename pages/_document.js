fc695b49890834b3ffe09b4442777a89317695b3
Benjamin <1234hteimaj4321@gmail.com>
2021-11-12 01:18:40 -0700

    chore: optimize

--- pages/_document.js
+++ pages/_document.js
@@ -3,13 +3,17 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
             <link 
                 href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Quicksand:wght@300;400;500;600;700&display=swap" 
                 rel="stylesheet" />
+            <meta name="description" content="Benjamin Scott, Sofware Engineer" />
+            <meta name="keywords" content="Benjamin Scott, Software Engineer, Developer" />
+            <meta name="author" content="Benjamin Scott" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         </Head>
         <body>
           <Main />
