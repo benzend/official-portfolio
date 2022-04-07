c085f87ec23c17b3ede6b75bf464bf42f8d83bfc
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2022-04-07 04:41:36 -0600

    feat: typescript support added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import { BLogo } from '../components/svg/BLogo'
-import styles from '../styles/Home.module.scss'
+import Head from 'next/head';
+import { BLogo } from '../components/svg/BLogo';
+import styles from '../styles/Home.module.scss';
 
 export default function Home() {
   return (
@@ -18,20 +18,23 @@ export default function Home() {
             <h1 className={styles.mainTitle}>Benjamin Scott</h1>
             <p className={styles.subHeader}>Sofware Engineer</p>
             <div className={styles.iconsContainer}>
-              <i aria-hidden className={"fab fa-react " + styles.iconReact}></i>
-              <i aria-hidden className={"fab fa-js-square " + styles.iconJS}></i>
-              <i aria-hidden className={"fas fa-code " + styles.iconCode}></i>
-              <i aria-hidden className={"fab fa-python " + styles.iconPy}></i>
+              <i aria-hidden className={'fab fa-react ' + styles.iconReact}></i>
+              <i
+                aria-hidden
+                className={'fab fa-js-square ' + styles.iconJS}
+              ></i>
+              <i aria-hidden className={'fas fa-code ' + styles.iconCode}></i>
+              <i aria-hidden className={'fab fa-python ' + styles.iconPy}></i>
             </div>
           </div>
           <div className={styles.getInTouch}>
             <a href="mailto: benjamin.scottt.dev@gmail.com">Get In Touch</a>
           </div>
           <div className={styles.heroLogo}>
-            <BLogo fill='#383838' height="500px" width="500px" />
+            <BLogo fill="#383838" height="500px" width="500px" />
           </div>
         </section>
       </main>
     </div>
-  )
+  );
 }
