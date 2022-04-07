c1e786e1b037606c561220bebe7ab8bea26feda7
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2022-04-07 04:56:16 -0600

    chore: converted files to scss

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
-import Script from 'next/script'
-import '../styles/globals.css'
+import Script from 'next/script';
+import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script src="https://kit.fontawesome.com/dfd07826ac.js" crossOrigin="anonymous"></Script>
+      <Script
+        src="https://kit.fontawesome.com/dfd07826ac.js"
+        crossOrigin="anonymous"
+      ></Script>
       <Component {...pageProps} />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
