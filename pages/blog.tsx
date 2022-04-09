19a9fdca6f3bc768f0f3a294c2b95fa734f21bf4
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2022-04-09 16:27:40 -0600

    chore: moved date to top of posts in blogpage

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -19,35 +19,37 @@ export default function Blog({ posts }: IBlogProps) {
       <div>
         {posts.map((post) => (
           <div key={post.title} className="mb-10">
-            <Link href={`/posts/${post.slug}`}>
-              <h2 className="text-2xl md:text-3xl font-bold tracking-tighter leading-tight md:leading-none mb-4 hover:underline cursor-pointer">
-                {post.title}
-              </h2>
-            </Link>
-            <p className="mb-4">{post.excerpt}</p>
-
-            <div className="flex flex-col sm:flex-row sm:items-center sm:gap-x-5">
-              <Avatar
-                name={post.author.name}
-                picture={post.author.picture}
-                customStyles={{
-                  picture: { marginRight: '1rem', width: 30, height: 30 },
-                  name: {
-                    fontWeight: '500',
-                    fontSize: '1rem',
-                    fontFamily: "'Quicksand', sans-serif",
-                  },
-                }}
-              />
+            <div className="mb-2">
               <DateFormatter
                 dateString={post.date}
                 customStyles={{
                   fontStyle: 'italic',
                   fontWeight: 300,
                   color: '#666',
+                  fontSize: '.8rem',
                 }}
               />
             </div>
+            <Link href={`/posts/${post.slug}`}>
+              <h2 className="text-2xl md:text-3xl font-bold tracking-tighter leading-tight md:leading-none mb-4 hover:underline cursor-pointer">
+                {post.title}
+              </h2>
+            </Link>
+
+            <p className="mb-4">{post.excerpt}</p>
+
+            <Avatar
+              name={post.author.name}
+              picture={post.author.picture}
+              customStyles={{
+                picture: { marginRight: '1rem', width: 30, height: 30 },
+                name: {
+                  fontWeight: '500',
+                  fontSize: '1rem',
+                  fontFamily: "'Quicksand', sans-serif",
+                },
+              }}
+            />
           </div>
         ))}
       </div>
