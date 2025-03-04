77672582074c68986808c909f6f4c83b567e1a39
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2025-03-03 19:58:43 -0700

    feat: add intercept script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,10 @@ function MyApp({ Component, pageProps }) {
         src="https://kit.fontawesome.com/dfd07826ac.js"
         crossOrigin="anonymous"
       ></Script>
+
+      {/** Start of Helio Embed Code --> */}
+       <Script defer id="helioEmbedLoader" src="https://embed.helio.app/load.js?i=01BDZ8DDVXK6S1DW9S5YXTR5R2"></Script>
+      {/** End of Helio Embed Code --> */}
       <Component {...pageProps} />
     </>
   );
