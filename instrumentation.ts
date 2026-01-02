748a0406af44fedd8fed6fb2ab2b3560b15f0cac
Ben <benjamin.scottt.dev@gmail.com>
2026-01-02 11:08:49 -0700

    fix: vemetric

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,7 +1,9 @@
 import { vemetric } from '@vemetric/web';
 
 export function register() {
-  vemetric.init({
-    token: process.env.NEXT_PUBLIC_VEMETRIC_TOKEN!,
-  });
+  if (typeof window !== 'undefined') {
+    vemetric.init({
+      token: process.env.NEXT_PUBLIC_VEMETRIC_TOKEN!,
+    });
+  }
 }
